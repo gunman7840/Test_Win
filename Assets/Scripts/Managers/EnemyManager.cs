d6d0d976aa327678839ebf174c72047cceb94770
Ivan Gorbatsevich <gunman7840@gmail.com>
2015-09-22 16:41:34 +0300

    add managers

---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        EventManager.OnTargetReached += GameOver; //Подписываемся на событие(Добавляем метод который будет исполнятся по этому событию)
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+
+    void GameOver(GameObject enemy)
+    {
+        Debug.Log("--------------GameOver---------------");
+        Destroy(enemy);
+    }
+
+
+
+}
